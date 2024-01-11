56aa79e7cad01cb2caaedcf765692529f7e2bf0e
Felipe Duarte <feduarte.dev@gmail.com>
2024-01-11 11:59:34 -0300

    requisito 6 pronto

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,7 @@
+import { CartItem } from './types/CartItem';
+import { Food } from './types/Food';
+import { Drink } from './types/Drink';
+
 export function first<Type>(products: Type[]): Type {
   return products[0];
 }
@@ -10,4 +14,12 @@ export function updateItem<Type>(products: Type[], itemId: number, updatedData: 
   updatedProducts[itemId] = updatedData;
 
   return updatedProducts;
+}
+
+export function buildCartItem(product: Food | Drink, quantity: number): CartItem {
+  return {
+    name: product.name,
+    price: product.price,
+    quantity,
+  } as CartItem;
 }
